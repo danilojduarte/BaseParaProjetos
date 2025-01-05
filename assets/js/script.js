6e0b50647b46053fc69f22fceb7b7f70c14e1ce1
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-01-05 16:24:25 -0300

    Criando estrutura do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Exemplo de script básico para interatividade
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#hero button");
+  button.addEventListener("click", () => {
+      alert("Saiba mais sobre nosso projeto!");
+  });
+});
